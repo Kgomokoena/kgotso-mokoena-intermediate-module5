bf4eda1b56061b9b79c85c91069b30ff36a434b6
Mac <kgomokoena16@yahoo.com>
2022-09-15 04:05:08 +0200

    pulling data for the flag image and country information

--- App.tsx
+++ App.tsx
@@ -6,12 +6,24 @@
  */
 
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, Image, StyleSheet} from 'react-native';
+import * as data from './data/countries.json';
 
 const App = () => {
+  let randomCountry = Math.floor(Math.random() * 196); //196 countries in the world
+  const countryName = data[randomCountry].name;
+  const countryCapital = data[randomCountry].capital;
+  const countryRegion = data[randomCountry].region;
+  const countryCode = data[randomCountry].alpha3Code;
+  const imageUri = 'https://countryflagsapi.com/png/' + countryCode;
+
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>Country Name: {countryName}</Text>
+      <Text>Capital City: {countryCapital}</Text>
+      <Text>Region: {countryRegion}</Text>
+      <Text>Country Code: {countryCode}</Text>
+      <Image style={styles.flag} source={{uri: imageUri}} />
       <Button title="Submit" />
     </View>
   );
@@ -22,6 +34,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  flag: {
+    width: 300,
+    height: 250,
   },
 });
 
