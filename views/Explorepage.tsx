eca4551f8255e97e2ec7f56eb353c94b5a2105ed
Mac <kgomokoena16@yahoo.com>
2022-09-22 03:45:17 +0200

    explore page

--- views/Explorepage.tsx
+++ views/Explorepage.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, {useState} from 'react';
-import {View, Text, Button, Image, StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import * as data from '../data/countries.json';
 
 const Explorepage = () => {
@@ -17,26 +17,31 @@ const Explorepage = () => {
   const countryCapital = data[randomCountry].capital;
   const countryRegion = data[randomCountry].region;
   const countryCode = data[randomCountry].alpha3Code;
+  const callingCode = data[randomCountry].callingCodes;
   const imageUri = 'https://countryflagsapi.com/png/' + countryCode;
 
   return (
     <View style={styles.container}>
-      <Text>Country Name: {countryName}</Text>
-      <Text>Capital City: {countryCapital}</Text>
-      <Text>Region: {countryRegion}</Text>
-      <Text>Country Code: {countryCode}</Text>
+      <Text style={styles.heading}>Country: {countryName}</Text>
+      <Text style={styles.heading}>Capital City: {countryCapital}</Text>
+      <Text style={styles.heading}>Region: {countryRegion}</Text>
+      <Text style={styles.heading}>Country Code: {countryCode}</Text>
+      <Text style={styles.heading}>Calling Code: +{callingCode}</Text>
       <Image style={styles.flag} source={{uri: imageUri}} />
-      <Button
-        onPress={() => {
-          setrandomCountry(Math.floor(Math.random() * 196));
-        }}
-        title="Next"
-      />
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          onPress={() => {
+            setrandomCountry(Math.floor(Math.random() * 196));
+          }}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  heading: {fontWeight: 'bold', fontSize: 16},
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -48,6 +53,26 @@ const styles = StyleSheet.create({
     height: 200,
     resizeMode: 'center',
   },
+
+  buttonText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    alignSelf: 'center',
+  },
+
+  buttons: {
+    paddingHorizontal: 5,
+    paddingVertical: 3,
+    borderRadius: 10,
+    backgroundColor: '#64b5f6',
+    minWidth: '30%',
+    textAlign: 'center',
+    margin: 14,
+    alignItems: 'center',
+  },
+
+  buttonContainer: {flexDirection: 'row', margin: 30},
 });
 
 export default Explorepage;
